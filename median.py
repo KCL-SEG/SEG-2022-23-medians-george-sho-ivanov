869b23b9342132a92280f065d1f9e5cc20d8d0dc
George Ivanov <george.ivanov@kcl.ac.uk>
2022-10-21 16:32:51 +0100

    add functionality to method

--- median.py
+++ median.py
@@ -5,6 +5,11 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
+        if (len(numbers) % 2 == 1):
+            print(numbers[int((len(numbers)-1)/2)])
+        else:
+            print((numbers[int((len(numbers))/2)] + numbers[int((len(numbers))/2)-1])/2)
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
